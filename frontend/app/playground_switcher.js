55400dbdd896f83992ee68924cef3935af5aa593
Anirudh Konidala <kidskodingclub@gmail.com>
2025-02-26 17:06:18 -0600

    removed unessescary code

--- frontend/app/playground_switcher.js
+++ frontend/app/playground_switcher.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import Editor from "@monaco-editor/react";
 
 export default function PlaygroundSwitcher({ code, setCode }) {
     const [playgrounds, setPlaygrounds] = useState([
